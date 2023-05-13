4d2aae59e6f145187edb208fbb437273d6906ff2
Kerem <Kerem@ASUS>
2023-05-13 12:51:15 +0300

    Add project files.

---
+++ FormlarArasiVeriTransferi/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormlarArasiVeriTransferi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Bir form içerisinden (Form1) başka bir form (Form2) açmak istediğimizde öncelikle açmak istediğimiz formdan(Form2) bir nesne oluşturmamız gerekecek.
+
+            Form2 form2 = new Form2();
+
+            form2.BackColor = Color.CornflowerBlue;
+
+            // Aşağıda Form2 içinde tanımlı olan property'e textbox içindeki verimizi set ettik. Ve böylece Form1'de olan verimiz Form2'ye taşınmış oldu.
+
+            form2.Adi = textBox1.Text;
+
+            //form2.Show(); // Show() ile ilgili formu istediğimiz sayıda açabiliyoruz.
+
+            form2.ShowDialog(); // Form1'e dönmek için öncelikli olarak açılan Form2'yi kapatmamız gerekiyor.
+        }
+    }
+}
---
+++ FormlarArasiVeriTransferi/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormlarArasiVeriTransferi
+{
+    public partial class Form2 : Form
+    {
+
+        // Farklı bir form üzerinden bu forma bir veri taşımak istiyorsak, öncelikle class seviyesinde bir değişken ya da property tanımlamamız gerekiyor. Burada tek bir şart var o da tanımlanan değişken ya da kesinlikle public olmalı. Aksi takdirde bu değişkene ulaşamayız.
+        // Bu formu açmak istediğimiz noktada bu formdan bir instance/örnek/nesne almalıyız. Yani bu formu new'lememiz gerekir. Bu durumda burada tanımladığımız değişkene de ulaşabiliriz.
+
+        public string Adi { get; set; }
+
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // Property'e set edilen veriyi label'ın text'ine atarak kullanmış olduk. Farklı amaçlar için de bu şekilde bir veriyi farklı bir forma göndererek ihtiyaçlarımız doğrultusunda kullanabiliriz.
+
+            label2.Text = Adi;
+        }
+    }
+}
